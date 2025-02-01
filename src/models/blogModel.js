5c9f9e1a3a97f1c9975f1651b85491e4225c51bb
Khushi <khushichhonkare@gmail.com>
2025-02-01 12:51:13 +0530

    feat: added blog model

---
+++ src/models/blogModel.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+const blogSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Blog = model('Blog', blogSchema);
+
+export default Blog;
